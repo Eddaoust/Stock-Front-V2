e73e49e8b8414555eb5e3e24de047f7dfe8bb480
Eddaoust <edmond.daoust@gmail.com>
2020-02-16 19:40:33 +0100

    Product Add Action(3)

--- src/components/Products/ProductAdd.js
+++ src/components/Products/ProductAdd.js
@@ -1,21 +1,68 @@
-import React, {useEffect, useState} from "react";
-import classes from "../Category/CategoryAdd.module.css";
-import {Button, Grid, Input, Paper, TextField, Typography} from "@material-ui/core";
+import React, {useEffect, useState, useRef} from "react";
+import { makeStyles } from '@material-ui/core/styles';
+import {Button, Grid, Input, Paper, TextField, Typography, FormControl, InputLabel, Select, MenuItem, Container} from "@material-ui/core";
 import Rating from '@material-ui/lab/Rating';
 
+const useStyles = makeStyles(theme => ({
+    formControl: {
+        margin: theme.spacing(1),
+        minWidth: 220,
+    },
+    selectEmpty: {
+        marginTop: theme.spacing(2),
+    },
+    nested: {
+        paddingLeft: theme.spacing(4),
+    },
+}));
+
 function ProductAdd(props) {
+    // Clear error on unmount
     useEffect(() => {
         return () => {
             props.clearError()
         }
     }, [])
 
+    const classes = useStyles();
+
+
+    const inputLabel = useRef(null);
+    const [labelWidth, setLabelWidth] = useState(0);
+    React.useEffect(() => {
+        setLabelWidth(inputLabel.current.offsetWidth);
+    }, []);
+
+    // Focus the selected option
+    const [infoSelected, setInfoSelected] = useState('');
+    const handleChange = event => {
+        setInfoSelected(event.target.value);
+    };
+
+    // Build the nested option list
+    const menuItemsList = [];
+    if (props.category.data) {
+        props.category.data.map(category => {
+            menuItemsList.push(<MenuItem key={category.id} disabled>{category.name}</MenuItem>)
+            if (category.subCategories) {
+                category.subCategories.map(subCategory => {
+                    menuItemsList.push(<MenuItem key={subCategory.id} value={subCategory.id} className={classes.nested}>{subCategory.name}</MenuItem>)
+                })
+            }
+        })
+    } else {
+        menuItemsList.push(<MenuItem disabled>Create a category...</MenuItem> )
+    }
+
+
+    // Add info fields
     const [infoFields, setInfoFields] = useState([['key-0', 'value-0']]);
     const addInfoField = () => {
         if (infoFields.length < 5) {
             setInfoFields([...infoFields,[`key-${infoFields.length}`, `value-${infoFields.length}`]])
         }
-    }
+    };
+
 
     // Add fields error props
     let error = false;
@@ -54,74 +101,116 @@ function ProductAdd(props) {
                             event.preventDefault();
                             props.productCreate(event, props)
                         }}>
-                            <TextField
-                                name="name"
-                                variant="outlined"
-                                required
-                                fullWidth
-                                id="name"
-                                label="Nom"
-                                autoFocus
-                                type="text"
-                                error={error}
-                                helperText={helperText}
-                            />
-                            <TextField
-                                name="description"
-                                variant="outlined"
-                                required
-                                fullWidth
-                                multiline={true}
-                                rows={2}
-                                rowsMax={4}
-                                id="description"
-                                label="Description"
-                                type="text"
-                                error={error}
-                                helperText={helperText}
-                            />
-                            {infoFields.map(input => (
-                                <div key={input[0]}>
+                            <Grid container spacing={2}>
+                                <Grid item xs={12}>
                                     <TextField
+                                        name="name"
                                         variant="outlined"
+                                        required
+                                        fullWidth
+                                        id="name"
                                         label="Nom"
+                                        autoFocus
                                         type="text"
-                                        name={input[0]} />
+                                        error={error}
+                                        helperText={helperText}
+                                    />
+                                </Grid>
+                                <Grid item xs={12}>
                                     <TextField
+                                        name="description"
                                         variant="outlined"
-                                        label="Valeur"
+                                        required
+                                        fullWidth
+                                        multiline={true}
+                                        rows={2}
+                                        rowsMax={4}
+                                        id="description"
+                                        label="Description"
                                         type="text"
-                                        name={input[1]} />
-                                </div>
-                            ))}
-                            <Input
-                                type="file"
-                                name="image"
-                                variant="outlined"
-                                fullWidth
-                                id="image"
-                                label="Image"
-                            />
-                            <Rating
-                                name="simple-controlled"
-                            />
-                            <Button
-                                type="button"
-                                variant="contained"
-                                color="primary"
-                                onClick={addInfoField}
-                            >
-                                Ajouter un champ
-                            </Button>
-                            <Button
-                                type="submit"
-                                fullWidth
-                                variant="contained"
-                                color="primary"
-                                className={classes.Button}
-                            >
-                                Ajouter
-                            </Button>
+                                        error={error}
+                                        helperText={helperText}
+                                    />
+                                </Grid>
+
+                                {infoFields.map(input => (
+                                    <Grid item xs={10}>
+                                        <Grid container spacing={2} key={input[0]}>
+                                            <Grid item xs={5}>
+                                                <TextField
+                                                    variant="outlined"
+                                                    label="Nom"
+                                                    type="text"
+                                                    name={input[0]} />
+                                            </Grid>
+                                            <Grid item xs={5}>
+                                                <TextField
+                                                    variant="outlined"
+                                                    label="Valeur"
+                                                    type="text"
+                                                    name={input[1]} />
+                                            </Grid>
+                                        </Grid>
+                                    </Grid>
+
+                                ))}
+                                <Grid item xs={2}>
+                                    <Button
+                                        type="button"
+                                        variant="contained"
+                                        color="primary"
+                                        onClick={addInfoField}
+                                    >
+                                        Ajouter un champ
+                                    </Button>
+                                </Grid>
+                                <Grid item xs={6}>
+                                    {/* If category data is empty, display a create category button */}
+                                    {props.category.data ? (
+                                        <FormControl variant="outlined" className={classes.formControl}>
+                                            <InputLabel ref={inputLabel} id="demo-simple-select-outlined-label">
+                                                Infos Complémentaires
+                                            </InputLabel>
+                                            <Select
+                                                labelWidth={labelWidth}
+                                                value={infoSelected}
+                                                onChange={handleChange}
+
+                                            >
+                                                {menuItemsList}
+                                            </Select>
+                                        </FormControl>
+                                    ): (
+                                        <Button>Créer une Catégorie</Button>
+                                    )}
+                                </Grid>
+                                <Grid item xs={12}>
+                                    <Rating
+                                        name="simple-controlled"
+                                    />
+                                </Grid>
+                                <Grid item xs={12}>
+                                    <Input
+                                        type="file"
+                                        name="image"
+                                        variant="outlined"
+                                        fullWidth
+                                        id="image"
+                                        label="Image"
+                                    />
+                                </Grid>
+                                <Grid item xs={12}>
+                                    <Button
+                                        type="submit"
+                                        fullWidth
+                                        variant="contained"
+                                        color="primary"
+                                        className={classes.Button}
+                                    >
+                                        Ajouter
+                                    </Button>
+                                </Grid>
+                            </Grid>
                         </form>
                     </Grid>
                 </Grid>
